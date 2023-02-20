bc10a3ccc14b0bd886e28378afa247c8ad7ceefa
César Olvera <olvera_cesar.m@hotmail.com>
2023-02-20 16:43:10 -0600

    se creo funciones basicas

--- js/app.js
+++ js/app.js
@@ -121,3 +121,84 @@ function actualizarTabla() {
         tabla.appendChild(fila);
     });
 }
+
+function eliminar(indice) {
+    datos.splice(indice, 1); //elimina el dato del array
+
+    //guarda los datos actualizados del array
+    localStorage.setItem('datos',JSON.stringify(datos));
+
+    actualizarTabla();//Actualiza la tabla
+}
+
+function editar(indice) {
+    const dato = datos[indice];
+
+    document.querySelector('#nombre').value = dato.nombre;
+    document.querySelector('#email').value = dato.email;
+    document.querySelector('#telefono').value = dato.telefono;
+    document.querySelector('#sexo').value = dato.sexo;
+    document.querySelector('#especialidad').value = dato.especialidad;
+    document.querySelector('#fCita').value = dato.fCita;
+    document.querySelector('#observaciones').value = dato.observaciones;
+
+    //eliminar dato original 
+    datos.splice(indice, 1);
+
+    //guarda los datos actualizados
+    localStorage.setItem('datos', JSON.stringify(datos));
+
+    // Cambiar la función del botón "Agregar" para que actualice el dato en lugar de agregar uno nuevo
+    const botonAgregar = document.querySelector('#formulario button');
+    botonAgregar.innerHTML = 'Actualizar';
+    botonAgregar.removeEventListener('click', agregar);
+    botonAgregar.addEventListener('click', () => {
+        //Obtener los nuevos valores
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const sexo = document.querySelector('#sexo').value;
+        const especialidad = document.querySelector('#especialidad').value;
+        const fCita = document.querySelector('#fCita').value;
+        const observaciones = document.querySelector('#observaciones').value;
+        
+        //agregar los nuevos datos al array
+        datos.splice(indice, 0, {nombre: nombre, email: email, telefono: telefono, sexo: sexo, especialidad: especialidad, fCita: fCita, observaciones: observaciones});
+
+        //limpiar los campos del formulario
+        document.querySelector('#nombre').value = '';
+        document.querySelector('#email').value = '';
+        document.querySelector('#telefono').value = '';
+        document.querySelector('#sexo').value = '';
+        document.querySelector('#especialidad').value = '';
+        document.querySelector('#fCita').value = '';
+        document.querySelector('#observaciones').value = '';
+
+        //Cambiar la función del botón "Actualizar" para que agregue un nuevo dato en lugar de actualizar uno existente
+        botonAgregar.innerHTML = 'Agregar';
+        botonAgregar.removeEventListener('click', actualizar);
+        botonAgregar.addEventListener('click', agregar);
+
+        //guardar datos actualizados
+        localStorage.setItem('datos',JSON.stringify(datos));
+
+        //actualizar tabla
+        actualizarTabla();
+    });
+}
+actualizarTabla();
+/*
+function validarFecha() {
+    //obtener fecha actual
+    var fechaActual = new Date();
+
+    //Obtener la fecha seleccionada por usuario
+    var fechaSeleccionada = new Date(document.getElementById('fCita').value);
+
+    //comparar las fechas
+    if (fechaSeleccionada < fechaActual) {
+        alert('La fehca seleccionada no puede ser anterior a la actual.');
+        return false;
+    }
+}
+*/
